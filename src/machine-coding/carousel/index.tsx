127b4fe088531df97c93bf6fdb77f5c97b9117f4
Sumit Dey <sumit@betterhalf.ai>
2022-12-10 02:30:42 +0530

    remoced redundant events

--- src/machine-coding/carousel/index.tsx
+++ src/machine-coding/carousel/index.tsx
@@ -23,7 +23,8 @@ const Carousel = () => {
 
   const startXRef = useRef(0); // stores the starting x position of the touch event
 
-  const [isMouseDragEnabled, setIsMouseDragEnabled] = useState(false);
+  // const [isMouseDragEnabled, setIsMouseDragEnabled] = useState(false);
+  const isMouseDragEnabled = useRef(false);
 
   const NO_OF_CARDS = 5;
   const CARD_WIDTH = 320;
@@ -35,9 +36,14 @@ const Carousel = () => {
     console.log('onMouseDown fired');
     e.preventDefault();
 
-    setIsMouseDragEnabled(true);
+    // setIsMouseDragEnabled(true);
+    isMouseDragEnabled.current = true;
     currentOffsetXRef.current = offsetXRef.current as number;
     startXRef.current = e.clientX;
+
+    // We attached these two events in the window so that even if the mouse cursor goes out of the list element, it will still continue to fire both the mouse move and mouse up event.
+    window.addEventListener('mouseup', onMouseUp);
+    window.addEventListener('mousemove', onMouseMove);
   };
 
   const onMouseMove = (e: MouseEvent) => {
@@ -55,29 +61,60 @@ const Carousel = () => {
 
     offsetToBeSet < -(CARD_WIDTH * (NO_OF_CARDS - 1)) : this means that the list is swiped to the left most position , if you try to swipe further, translateX will keep on decreasing and which will make the list go out of the viewport
     why  (NO_OF_CARDS - 1) ?
-    the reason is when we swiped the list the list to the last card, then the the last card's right end is at the right most position of the viewport, so we are calculating the left most position of the last card by multiplying the width of each card with the number of cards - 1,
-  
+    the reason is when we swiped the list the list to the last card, then the the last card's right end is at
+    
+    
+
     if there are two cards and the width of each card is 320px, then the left most position will be -320px and the right most position will be 0px
+
     offsetToBeSet > 0 : this means that the list is swiped to the right most position, if you try to swipe further, translateX will keep on increasing and which will make the list go out of the viewport
 
     */
+    // const onMouseMove = (e: MouseEvent) => {
+    //   console.log('onMouseMove fired');
+
+    //   e.preventDefault();
+    //   if (!isMouseDragEnabled) return;
+
+    //   const diff = e.clientX - startXRef.current; // difference between  the current position and the starting position
+
+    //   const offsetToBeSet = currentOffsetXRef.current + diff * 1.5; // multiply the diff by 1.3 to make the movement faster and then add it to the current offset
+    //   console.log({ offsetToBeSet });
+
+    //   /* if the the swipe is extends the left or right boundary of the list, then we don't want to move the list
+
+    //   offsetToBeSet < -(CARD_WIDTH * (NO_OF_CARDS - 1)) : this means that the list is swiped to the left most position , if you try to swipe further, translateX will keep on decreasing and which will make the list go out of the viewport
+    //   why  (NO_OF_CARDS - 1) ?
+    //   the reason is when we swiped the list the list to the last card, then the the last card's right end is at the right most position of the viewport, so we are calculating the left most position of the last card by multiplying the width of each card with the number of cards - 1,
+
+    //   if there are two cards and the width of each card is 320px, then the left most position will be -320px and the right most position will be 0px
+    //   offsetToBeSet > 0 : this means that the list is swiped to the right most position, if you try to swipe further, translateX will keep on increasing and which will make the list go out of the viewport
+
+    //   */
+
+    //   if (offsetToBeSet < -(CARD_WIDTH * (NO_OF_CARDS - 1)) || offsetToBeSet > 0) {
+    //     return;
+    //   }
+
+    //   setOffsetX(offsetToBeSet);
+
+    //   // We attached these two events in the window so that even if the mouse cursor goes out of the list element, it will still continue to fire both the mouse move and mouse up event.
+    //   window.addEventListener('mouseup', onMouseUp);
+    //   window.addEventListener('mousemove', onMouseMove);
+    // };
 
     if (offsetToBeSet < -(CARD_WIDTH * (NO_OF_CARDS - 1)) || offsetToBeSet > 0) {
       return;
     }
 
     setOffsetX(offsetToBeSet);
-
-    // We attached these two events in the window so that even if the mouse cursor goes out of the list element, it will still continue to fire both the mouse move and mouse up event.
-    window.addEventListener('mouseup', onMouseUp);
-    window.addEventListener('mousemove', onMouseMove);
   };
 
   const onMouseUp = (e: MouseEvent) => {
     e.preventDefault();
     console.log('onMouseUp fired');
-    setIsMouseDragEnabled(false);
-
+    // setIsMouseDragEnabled(false);
+    isMouseDragEnabled.current = false;
     window.removeEventListener('mouseup', onMouseUp);
     window.removeEventListener('mousemove', onMouseMove);
   };
@@ -97,15 +134,15 @@ const Carousel = () => {
         }}
         // onTouchStart={handleTouchStart}
         onMouseDown={onMouseDown}
-        onMouseMove={onMouseMove}
-        onMouseUp={onMouseUp}
+        // onMouseMove={onMouseMove}
+        // onMouseUp={onMouseUp}
       >
         {/* Carousel Card Container */}
         {
           // all the cards as children in display flex (flex-direction: row) for horizontal carousel
           new Array(NO_OF_CARDS).fill(0).map((_, index) => (
             <div key={index} className='w-[320px] h-[200px]  flex-grow-0 flex-shrink-0 basis-full'>
-              <div className='grid w-full h-full text-2xl bg-blue-500 border-4 border-blue-900 pointer-events-none place-items-center'>
+              <div className='grid w-full h-full text-2xl bg-blue-700 border-2 border-yellow-300 pointer-events-none place-items-center'>
                 <span className='pointer-events-none'>{index}</span>
               </div>
             </div>
