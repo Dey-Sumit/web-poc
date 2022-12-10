a4c61d3f952cf96f9626b09b2e60659e39c60927
Sumit Dey <sumit@betterhalf.ai>
2022-12-10 17:33:11 +0530

    completed carousel

--- src/machine-coding/carousel/index.tsx
+++ src/machine-coding/carousel/index.tsx
@@ -14,6 +14,9 @@
 import { useRef, useState } from 'react';
 import { useStateRef } from './hooks';
 
+const NO_OF_CARDS = 5;
+const CARD_WIDTH = 320;
+
 const Carousel = () => {
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
 
@@ -23,11 +26,7 @@ const Carousel = () => {
 
   const startXRef = useRef(0); // stores the starting x position of the touch event
 
-  // const [isMouseDragEnabled, setIsMouseDragEnabled] = useState(false);
   const isMouseDragEnabled = useRef(false);
-
-  const NO_OF_CARDS = 5;
-  const CARD_WIDTH = 320;
 
   // DRAG SCENARIO : START -> onMouseDown -> onMouseMove -> onMouseUp -> END
 
@@ -40,7 +39,6 @@ const Carousel = () => {
     isMouseDragEnabled.current = true;
     currentOffsetXRef.current = offsetXRef.current as number;
     startXRef.current = e.clientX;
-
     // We attached these two events in the window so that even if the mouse cursor goes out of the list element, it will still continue to fire both the mouse move and mouse up event.
     window.addEventListener('mouseup', onMouseUp);
     window.addEventListener('mousemove', onMouseMove);
@@ -55,53 +53,18 @@ const Carousel = () => {
     const diff = e.clientX - startXRef.current; // difference between  the current position and the starting position
 
     const offsetToBeSet = currentOffsetXRef.current + diff * 1.5; // multiply the diff by 1.3 to make the movement faster and then add it to the current offset
-    console.log({ offsetToBeSet });
 
     /* if the the swipe is extends the left or right boundary of the list, then we don't want to move the list
 
-    offsetToBeSet < -(CARD_WIDTH * (NO_OF_CARDS - 1)) : this means that the list is swiped to the left most position , if you try to swipe further, translateX will keep on decreasing and which will make the list go out of the viewport
-    why  (NO_OF_CARDS - 1) ?
-    the reason is when we swiped the list the list to the last card, then the the last card's right end is at
-    
-    
+      offsetToBeSet < -(CARD_WIDTH * (NO_OF_CARDS - 1)) : this means that the list is swiped to the left most position , if you try to swipe further, translateX will keep on decreasing and which will make the list go out of the viewport
+      why  (NO_OF_CARDS - 1) ?
+      the reason is when we swiped the list the list to the last card, then the the last card's right end is at
 
-    if there are two cards and the width of each card is 320px, then the left most position will be -320px and the right most position will be 0px
+      if there are two cards and the width of each card is 320px, then the left most position will be -320px and the right most position will be 0px
 
-    offsetToBeSet > 0 : this means that the list is swiped to the right most position, if you try to swipe further, translateX will keep on increasing and which will make the list go out of the viewport
+      offsetToBeSet > 0 : this means that the list is swiped to the right most position, if you try to swipe further, translateX will keep on increasing and which will make the list go out of the viewport
 
-    */
-    // const onMouseMove = (e: MouseEvent) => {
-    //   console.log('onMouseMove fired');
-
-    //   e.preventDefault();
-    //   if (!isMouseDragEnabled) return;
-
-    //   const diff = e.clientX - startXRef.current; // difference between  the current position and the starting position
-
-    //   const offsetToBeSet = currentOffsetXRef.current + diff * 1.5; // multiply the diff by 1.3 to make the movement faster and then add it to the current offset
-    //   console.log({ offsetToBeSet });
-
-    //   /* if the the swipe is extends the left or right boundary of the list, then we don't want to move the list
-
-    //   offsetToBeSet < -(CARD_WIDTH * (NO_OF_CARDS - 1)) : this means that the list is swiped to the left most position , if you try to swipe further, translateX will keep on decreasing and which will make the list go out of the viewport
-    //   why  (NO_OF_CARDS - 1) ?
-    //   the reason is when we swiped the list the list to the last card, then the the last card's right end is at the right most position of the viewport, so we are calculating the left most position of the last card by multiplying the width of each card with the number of cards - 1,
-
-    //   if there are two cards and the width of each card is 320px, then the left most position will be -320px and the right most position will be 0px
-    //   offsetToBeSet > 0 : this means that the list is swiped to the right most position, if you try to swipe further, translateX will keep on increasing and which will make the list go out of the viewport
-
-    //   */
-
-    //   if (offsetToBeSet < -(CARD_WIDTH * (NO_OF_CARDS - 1)) || offsetToBeSet > 0) {
-    //     return;
-    //   }
-
-    //   setOffsetX(offsetToBeSet);
-
-    //   // We attached these two events in the window so that even if the mouse cursor goes out of the list element, it will still continue to fire both the mouse move and mouse up event.
-    //   window.addEventListener('mouseup', onMouseUp);
-    //   window.addEventListener('mousemove', onMouseMove);
-    // };
+      */
 
     if (offsetToBeSet < -(CARD_WIDTH * (NO_OF_CARDS - 1)) || offsetToBeSet > 0) {
       return;
@@ -119,23 +82,19 @@ const Carousel = () => {
     window.removeEventListener('mousemove', onMouseMove);
   };
 
-  1;
   return (
     //  Container
     <div className='w-[320px] h-[200px]   overflow-hidden relative'>
-      {/* Viewport */}
-      {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
       <div
         className='flex '
         style={{
-          // transform: `translateX(-${selectedIndex * 320}px)`,
           transform: `translate3d(${offsetX}px, 0, 0)`,
           transition: 'transform 0.5s ease-in-out',
         }}
-        // onTouchStart={handleTouchStart}
         onMouseDown={onMouseDown}
-        // onMouseMove={onMouseMove}
-        // onMouseUp={onMouseUp}
+        // onMouseDown={onTouchStart}
+        role='button'
+        tabIndex={0}
       >
         {/* Carousel Card Container */}
         {
